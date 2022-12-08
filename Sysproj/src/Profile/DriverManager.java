d7e372433097f9ed240726b563d848b9d5be3f8f
qrowcrowx <102508114+qrowcrowx@users.noreply.github.com>
2022-12-08 13:36:48 +0800

    Add files via upload

---
+++ Sysproj/src/Profile/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Profile;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author vince
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqldatabase1cwcul3ylqcmqapsoutheast, String admin, String zxcasdqwe123) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
